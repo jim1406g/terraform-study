4bcc815e8805fa0c02b30dc2bdf30b64d4ff8f5d
jim1406 <jim1406g@gmail.com>
2026-04-21 12:54:26 +0600

    Trfm-94 : Configurable web server

--- main.tf
+++ main.tf
@@ -14,6 +14,31 @@ provider "yandex" {
   service_account_key_file = "/home/jim1406/.config/yandex-cloud/jim1406-sa-key.json"
 }
 
+variable "server_port" {
+  description = "Port for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+resource "yandex_vpc_security_group" "instance" {
+  name = "terraform-example-instance"
+  # default
+  network_id = "enpuge6idiit2v65es7o"
+
+  ingress {
+    protocol       = "TCP"
+    description    = "WEB"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = var.server_port
+  }
+
+  egress {
+    description    = "Permit ANY"
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "yandex_compute_instance" "example" {
   name        = "terraform-example"
   platform_id = "standard-v1"
@@ -35,16 +60,21 @@ resource "yandex_compute_instance" "example" {
           content: |
             #!/bin/bash
             echo "Hello World!"
+            cat /opt/init_script.sh
             echo "Hello, World!" > index.html
-            nohup busybox httpd -f -p 8080 &
+            nohup busybox httpd -f -p $${1:-8080} &
           permissions: '0755'
+      bootcmd:
+        - /opt/init_script.sh ${var.server_port}
       runcmd:
-        - /opt/init_script.sh
+        - /opt/init_script.sh ${var.server_port}
     EOF
   }
 
   network_interface {
-    nat       = true
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.instance.id]
+    # default-ru-central1-b
     subnet_id = "e2l5i4p8utd89i0ad456"
   }
 
@@ -57,4 +87,9 @@ resource "yandex_compute_instance" "example" {
   scheduling_policy {
     preemptible = true
   }
+}
+
+output "public_ip" {
+  description = "The public IP address of the web server"
+  value       = yandex_compute_instance.example.network_interface[0].nat_ip_address
 }

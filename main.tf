437992b76946bac2b07579749789d7d1ed383327
jim1406 <jim1406g@gmail.com>
2026-04-22 20:23:06 +0600

    Trfm-99 : Cluster of web servers

--- main.tf
+++ main.tf
@@ -7,23 +7,35 @@ terraform {
   required_version = ">= 0.13"
 }
 
+variable "service_account" {
+  default = {
+    id       = "ajenc66cncovv74n6act"
+    key_file = "/home/jim1406/.config/yandex-cloud/jim1406-sa-key.json"
+  }
+  description = "yandex_iam_service_account"
+  type        = map(string)
+}
+
 provider "yandex" {
   zone                     = "ru-central1-b"
   cloud_id                 = "b1gv7nnhp4nje6bct6la"
   folder_id                = "b1g197129s2l7e4j8o6s"
-  service_account_key_file = "/home/jim1406/.config/yandex-cloud/jim1406-sa-key.json"
+  service_account_key_file = var.service_account.key_file
 }
 
 variable "server_port" {
+  default     = 8080
   description = "Port for HTTP requests"
   type        = number
-  default     = 8080
+}
+
+data "yandex_vpc_network" "net" {
+  name = "default"
 }
 
 resource "yandex_vpc_security_group" "instance" {
-  name = "terraform-example-instance"
-  # default
-  network_id = "enpuge6idiit2v65es7o"
+  name       = "terraform-example-instance"
+  network_id = data.yandex_vpc_network.net.id
 
   ingress {
     protocol       = "TCP"
@@ -39,19 +51,36 @@ resource "yandex_vpc_security_group" "instance" {
   }
 }
 
-resource "yandex_compute_instance" "example" {
-  name        = "terraform-example"
-  platform_id = "standard-v1"
 
-  boot_disk {
-    initialize_params {
-      # ubuntu-24-04-lts-v20260413 (yc compute image list --folder-id standard-images)
-      image_id = "fd83esfomhq25p2ono90"
-    }
+resource "yandex_compute_instance_group" "example" {
+  name               = "terraform-example-ig"
+  service_account_id = var.service_account.id
+
+  allocation_policy {
+    zones = ["ru-central1-a", "ru-central1-b"]
   }
 
-  metadata = {
-    user-data = <<-EOF
+  deploy_policy {
+    max_creating     = 2
+    max_deleting     = 2
+    max_expansion    = 2
+    max_unavailable  = 1
+    startup_duration = 120
+  }
+
+  instance_template {
+    name        = "terraform-example-{instance.index}"
+    platform_id = "standard-v1"
+
+    boot_disk {
+      initialize_params {
+        # ubuntu-24-04-lts-v20260413 (yc compute image list --folder-id standard-images)
+        image_id = "fd83esfomhq25p2ono90"
+      }
+    }
+
+    metadata = {
+      user-data = <<-EOF
       #cloud-config
       packages:
         - busybox
@@ -69,27 +98,34 @@ resource "yandex_compute_instance" "example" {
       runcmd:
         - /opt/init_script.sh ${var.server_port}
     EOF
+    }
+
+    network_interface {
+      nat                = true
+      network_id         = data.yandex_vpc_network.net.id
+      security_group_ids = [yandex_vpc_security_group.instance.id]
+      subnet_ids         = data.yandex_vpc_network.net.subnet_ids
+    }
+
+    resources {
+      core_fraction = 5
+      cores         = 2
+      memory        = 4
+    }
+
+    scheduling_policy {
+      preemptible = true
+    }
   }
 
-  network_interface {
-    nat                = true
-    security_group_ids = [yandex_vpc_security_group.instance.id]
-    # default-ru-central1-b
-    subnet_id = "e2l5i4p8utd89i0ad456"
-  }
-
-  resources {
-    core_fraction = 5
-    cores         = 2
-    memory        = 4
-  }
-
-  scheduling_policy {
-    preemptible = true
+  scale_policy {
+    fixed_scale {
+      size = 2
+    }
   }
 }
 
 output "public_ip" {
-  description = "The public IP address of the web server"
-  value       = yandex_compute_instance.example.network_interface[0].nat_ip_address
+  description = "The public IP addresses of the web servers"
+  value       = yandex_compute_instance_group.example.instances.*.network_interface.0.nat_ip_address
 }

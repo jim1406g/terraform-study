b192a934a41d7bb58082744c898a1f2aacd6773f
jim1406 <jim1406g@gmail.com>
2026-05-08 23:17:59 +0600

    Trfm-119 : Terraform state (S3)

--- main.tf
+++ main.tf
@@ -4,7 +4,29 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 1.6.3"
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    bucket = "terraform-up-and-running-state"
+    region = "ru-central1"
+    key    = "terraform-study/terraform.tfstate"
+    # Статический ключ доступа для сервисного аккаунта (AWS-совместимый)
+    # mkdir -p ~/.aws
+    # cat > ~/.aws/credentials << EOF
+    # [jim1406-sa]
+    # aws_access_key_id = YCA...
+    # aws_secret_access_key = YCT...
+    # EOF
+    profile = "jim1406-sa"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+  }
 }
 
 data "yandex_vpc_network" "net" {
@@ -140,7 +162,7 @@ resource "yandex_compute_instance_group" "example" {
     }
 
     network_interface {
-      nat                = true
+      nat                = false
       network_id         = data.yandex_vpc_network.net.id
       security_group_ids = [yandex_vpc_security_group.instance.id]
       subnet_ids         = data.yandex_vpc_network.net.subnet_ids

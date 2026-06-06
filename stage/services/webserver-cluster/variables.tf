b9a3d9b672635ec135c58d4062609dc5451abac3
jim1406 <jim1406g@gmail.com>
2026-06-06 12:24:33 +0600

    Trfm-132 : Terraform state (isolation)

---
+++ stage/services/webserver-cluster/variables.tf
@@ -0,0 +1,31 @@
+variable "service_account" {
+  default = {
+    id       = "ajenc66cncovv74n6act"
+    key_file = "/home/jim1406/.config/yandex-cloud/jim1406-sa-key.json"
+  }
+  description = "yandex_iam_service_account"
+  type        = map(string)
+}
+
+variable "zone_subnet" {
+  default = {
+    zone0 = {
+      zone      = "ru-central1-b"
+      subnet    = "default-ru-central1-b"
+      subnet_id = "e2l5i4p8utd89i0ad456"
+    }
+    zone1 = {
+      zone      = "ru-central1-a"
+      subnet    = "default-ru-central1-a"
+      subnet_id = "e9btg82r2u94c1432bbn"
+    }
+  }
+  description = "Zones and Subnets"
+  type        = map(map(string))
+}
+
+variable "server_port" {
+  default     = 8080
+  description = "Port for HTTP requests"
+  type        = number
+}

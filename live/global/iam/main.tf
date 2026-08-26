35f6cc575697f07b6c56e39a94e4dea640896f63
jim1406 <jim1406g@gmail.com>
2026-08-26 17:44:13 +0600

    Trfm-177 : Loop for_each (IAM)

--- live/global/iam/main.tf
+++ live/global/iam/main.tf
@@ -1,4 +1,4 @@
 resource "yandex_iam_service_account" "example" {
-  count = length(var.example_user_names)
-  name  = var.example_user_names[count.index]
+  for_each = toset(var.example_user_names)
+  name     = each.value
 }
--- live/global/iam/outputs.tf
+++ live/global/iam/outputs.tf
@@ -1,4 +1,9 @@
+output "all_sa" {
+  value       = yandex_iam_service_account.example
+  description = "The IDs of all SA"
+}
+
 output "all_sa_ids" {
-  value       = yandex_iam_service_account.example[*].id
+  value       = values(yandex_iam_service_account.example)[*].id
   description = "The IDs of all SA"
 }

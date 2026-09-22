afc0b62630b13fc93f4db8d8a5efafa2ad01ce7c
jim1406 <jim1406g@gmail.com>
2026-09-22 14:31:29 +0600

    Trfm-217 : Resume

--- live/global/iam/main.tf
+++ live/global/iam/main.tf
@@ -2,3 +2,8 @@ resource "yandex_iam_service_account" "example" {
   for_each = toset(var.example_user_names)
   name     = each.value
 }
+
+resource "yandex_iam_service_account" "existing_user" {
+  description = "Для теста terraform import"
+  name = "yevgeniy-brikman"
+}
